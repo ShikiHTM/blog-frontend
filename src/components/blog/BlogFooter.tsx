656501173b9f95bbea5b6c7a82e588f45d0e17c8
ShikiHTM <kizukusan123@gmail.com>
2026-05-24 01:48:10 +0700

    feat(blog-footer): add underline animation on return home button

--- src/components/blog/BlogFooter.tsx
+++ src/components/blog/BlogFooter.tsx
@@ -11,7 +11,10 @@ const BlogFooter: React.FC<BlogFooterProps> = ({ author }) => {
         <div className='w-full flex justify-between items-center'>
             <Link href={'/'} className='flex group items-center text-muted hover:text-text transition-colors gap-2'>
                 <FaArrowLeft size={15} className='group-hover:-translate-x-1 transition-transform' />
-                <span>Back to Home</span>
+                <span >
+                    Back to Home
+                    <div className='bg-text h-px w-0 group-hover:w-full transition-all duration-500 ease-out'></div>
+                </span>
             </Link>
             <div className='flex justify-center items-center gap-1'>
                 <span className='text-muted'>Written by </span>
