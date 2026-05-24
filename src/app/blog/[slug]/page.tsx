6bd1c0f870a118f0038366d32d98129d13ff4d08
ShikiHTM <kizukusan123@gmail.com>
2026-05-24 20:03:24 +0700

    chore: typo and format

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,22 +1,22 @@
-import { api } from '@/lib/ky'
-import React from 'react'
-import { ApiResponse } from '@/types/api.types'
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import { notFound } from 'next/navigation'
-import { mdxOptions } from '@/lib/mdx'
-import { mdxComponents } from '@/components/mdx/components'
-import { BlogHeader } from '@/components/blog/BlogHeader'
-import { TocSidebar } from '@/components/blog/toc/TocSidebar'
-import { extractToc } from '@/lib/toc'
-import BlogFooter from '@/components/blog/BlogFooter'
+import { api } from '@/lib/ky';
+import React from 'react';
+import { ApiResponse } from '@/types/api.types';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { notFound } from 'next/navigation';
+import { mdxOptions } from '@/lib/mdx';
+import { mdxComponents } from '@/components/mdx/components';
+import { BlogHeader } from '@/components/blog/BlogHeader';
+import { TocSidebar } from '@/components/blog/toc/TocSidebar';
+import { extractToc } from '@/lib/toc';
+import BlogFooter from '@/components/blog/BlogFooter';
 
 interface BlogPageProp {
-    params: Promise<{ slug: string }>
+    params: Promise<{ slug: string }>;
 }
 
 const BlogPage: React.FC<BlogPageProp> = async ({ params }) => {
     const response = await api.get(`posts/${(await params).slug}`, {
-        throwHttpErrors: false
+        throwHttpErrors: false,
     });
 
     if (response.status === 404) {
@@ -27,7 +27,7 @@ const BlogPage: React.FC<BlogPageProp> = async ({ params }) => {
     const toc = extractToc(blogData.content ?? '');
 
     return (
-        <div className="flex-1 flex flex-col w-full max-w-4xl mx-auto p-3">
+        <div className='flex-1 flex flex-col w-full max-w-4xl mx-auto p-3'>
             <BlogHeader title={blogData.title} date={blogData.created_at} cover={blogData.cover} />
             <div className='w-full h-px bg-zinc-600 dark:bg-zinc-400 my-6 items-center mx-auto' />
             <div className='grid xl:grid-cols-[1fr_15rem] xl:gap-10'>
@@ -43,7 +43,7 @@ const BlogPage: React.FC<BlogPageProp> = async ({ params }) => {
             <div className='w-full h-px bg-zinc-600 dark:bg-zinc-400 my-6 items-center mx-auto' />
             <BlogFooter author={blogData.author} />
         </div>
-    )
-}
+    );
+};
 
-export default BlogPage
+export default BlogPage;
