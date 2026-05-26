17e9c80ace22e68952d6920f01c1cd821342c018
ShikiHTM <kizukusan123@gmail.com>
2026-05-26 16:51:00 +0700

    feat: change font from NotoSerif to PlutoSans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,36 @@
-import type { Metadata } from "next";
-import { Noto_Serif, Noto_Serif_Display } from "next/font/google";
-import localFont from "next/font/local";
-import { ThemeProvider } from "next-themes";
-import "./globals.css";
-import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
-import { cn } from "@/lib/utils";
-
-const notoSerif = Noto_Serif({
-    subsets: ["vietnamese"],
-    weight: ["400", "600", "700"],
-    variable: "--font-noto-serif"
-})
-
-const notoDisplay = Noto_Serif_Display({
-    subsets: ["vietnamese"],
-    weight: ["500", "700", "800"],
-    variable: "--font-display"
-})
+import type { Metadata } from 'next';
+import { Noto_Serif, Noto_Serif_Display } from 'next/font/google';
+import localFont from 'next/font/local';
+import { ThemeProvider } from 'next-themes';
+import './globals.css';
+import Navbar from '@/components/layout/navbar';
+import Footer from '@/components/layout/footer';
+import { cn } from '@/lib/utils';
 
 const blexMono = localFont({
     src: './fonts/BlexMonoNerdFont-Regular.woff2',
-    variable: "--font-blex-mono",
-    display: 'swap'
+    variable: '--font-blex-mono',
+    display: 'swap',
+});
+
+const plutoLight = localFont({
+    src: './fonts/PlutoSans-CondLight.woff2',
+    variable: '--font-pluto-sans',
+});
+
+const plutoRegular = localFont({
+    src: './fonts/PlutoSans-CondRegular.woff2',
+    variable: '--font-pluto-header'
+});
+
+const plutoMedium = localFont({
+    src: './fonts/PlutoSans-CondMedium.woff2',
+    variable: '--font-pluto-display'
 })
 
 export const metadata: Metadata = {
     title: "Shiki's Blog",
-    description: "Shiki personal blog",
+    description: 'Shiki personal blog',
 };
 
 export default function RootLayout({
@@ -37,15 +40,14 @@ export default function RootLayout({
 }>) {
     return (
         <html
-            lang="vi" suppressHydrationWarning
-            className={cn("h-full", "antialiased", notoSerif.variable, notoDisplay.variable, blexMono.variable)}
+            lang='vi'
+            suppressHydrationWarning
+            className={cn('h-full', 'antialiased', plutoLight.variable, plutoRegular.variable, plutoMedium.variable, blexMono.variable)}
         >
-            <body className="min-h-screen flex flex-col">
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <body className='min-h-screen flex flex-col'>
+                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
                     <Navbar />
-                    <main className="flex-1 flex flex-col">
-                        {children}
-                    </main>
+                    <main className='flex-1 flex flex-col'>{children}</main>
                     <Footer />
                 </ThemeProvider>
             </body>
