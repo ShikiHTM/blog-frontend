fbe7e18ec4e99458c27c2f0e41d2e7b10fec30e2
ShikiHTM <kizukusan123@gmail.com>
2026-05-26 17:17:20 +0700

    feat: add line-spacing and temporary disable cover

--- src/components/blog/BlogHeader.tsx
+++ src/components/blog/BlogHeader.tsx
@@ -12,8 +12,9 @@ export const BlogHeader: React.FC<BlogHeaderProps> = ({ title, date, cover }) =>
     return (
         <header className='pt-5 flex flex-col gap-6'>
             <span className='text-muted'>{format(date, "MMMM do, yyyy")}</span>
-            <span className='font-display font-bold text-5xl'>{title}</span>
+            <span className='font-display font-bold text-4xl/6'>{title}</span>
 
+            {/*
             <div className='relative aspect-16/6 w-full'>
                 <Image
                     src={cover ?? ""}
@@ -22,6 +23,7 @@ export const BlogHeader: React.FC<BlogHeaderProps> = ({ title, date, cover }) =>
                     className='object-cover rounded-xl'
                 />
             </div>
+            */}
         </header>
     )
 }
